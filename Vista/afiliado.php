a2910ff36617b3ee3190a2d93a8468dc125f5dfe
alexmontanadiaz <alexmontanad@gmail.com>
2022-08-02 22:04:53 -0500

    se agrego header a afiliados

--- Vista/afiliado.php
+++ Vista/afiliado.php
@@ -11,28 +11,30 @@
 
     </style>
 </head>
+
 <body>
-
-<div class="contenedor">
-
-    <img src="https://www.ecommerce-nation.es/wp-content/uploads/2018/05/envio-gratuito-ecommerce-1000x640.jpg"
-        width="650px">
-    <h2>Clientes</h2>
-
-    <p>
-    <h3> Conductores</h3>
-    <p> resgitarte para ser parte de nuestro equipo</p>
-    <button class="boton"><span></span> registrar</button>
-    </p>
-
+    <?php require 'header.php';?>
     <div class="contenedor">
 
-        <img src="images/camion.png">
+        <img src="https://www.ecommerce-nation.es/wp-content/uploads/2018/05/envio-gratuito-ecommerce-1000x640.jpg"
+            width="650px">
+        <h2>Clientes</h2>
 
         <p>
-        <h3> actualiza tu ubicacion</h3>
-        <p> realiza la actualizacion de la ubicacion de tu automovil</p>
-        <button class="boton"><span></span> actualiza ubicacion</button>
+        <h3> Conductores</h3>
+        <p> resgitarte para ser parte de nuestro equipo</p>
+        <button class="boton"><span></span> registrar</button>
         </p>
 
-    </div>
+        <div class="contenedor">
+
+            <img src="images/camion.png">
+
+            <p>
+            <h3> actualiza tu ubicacion</h3>
+            <p> realiza la actualizacion de la ubicacion de tu automovil</p>
+            <button class="boton"><span></span> actualiza ubicacion</button>
+            </p>
+
+        </div>
+        <?php require 'footer.php';?>
